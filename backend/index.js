b95b7e7f8e8b5445c0e40611f8a2cedf86ecf626
muhammadessakakakhel <muhammadessakakakhel966@gmail.com>
2024-07-24 02:11:22 +0500

    Update index.js and other files

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST", "GET"],
+    credentials: true
+  }
+));
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser());
